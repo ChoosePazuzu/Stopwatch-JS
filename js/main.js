787e4da7c1343a4cf20991bb273f6876c21eddda
ChoosePazuzu <nduplessis13@gmail.com>
2022-09-07 13:47:06 +0200

    Added Styling

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 let minutes = 0;
 let seconds = 0;
 
+const intervalID = [];
+
 function countup(){
     seconds++;
     if (seconds === 60){
@@ -23,6 +25,15 @@ function countup(){
 
 function watch(state){
     if (state === "start"){
-        setInterval(countup,1000);
+        let interval = setInterval(countup,1000);
+        intervalID.push(interval)
+    }else if(state === "stop"){
+        for(i of intervalID){
+            clearInterval(i);
+        }
+        seconds = 00;
+        minutes = 00;
+        document.getElementById("min").innerHTML = "0" + minutes;
+        document.getElementById("sec").innerHTML = "0" + seconds;
     }
 }
